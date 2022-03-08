7cef44e247a61e08680e3feecbc016ef1bb61650
hari-acharya <95195181+hari-acharya@users.noreply.github.com>
2022-03-08 13:31:26 +0545

    Uploaded works of Falgun-24
    DKY SIR classes

---
+++ app.js
@@ -0,0 +1,8 @@
+//JavaScript File
+console.log("File Has been accessed");
+Function myFunction(){
+	console.log("Working");
+	//var name = ("Enter you name :");
+	//var address =("Enter you name :");
+	//var contactno =("Enter you name :");
+}
